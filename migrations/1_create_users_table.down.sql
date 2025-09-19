ee6c39d168bd32c6086353ed34087b3d35b6c56d
Crispy <chebunin19@gmail.com>
2025-09-19 22:24:44 +0300

    refactor(db): add lines to create_users_table migrations

--- migrations/1_create_users_table.down.sql
+++ migrations/1_create_users_table.down.sql
@@ -5,4 +5,5 @@ BEGIN
     IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
         DROP TYPE user_role;
     END IF;
-END$$;
+END
+$$;
--- migrations/1_create_users_table.up.sql
+++ migrations/1_create_users_table.up.sql
@@ -4,7 +4,8 @@ BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
         CREATE TYPE user_role AS ENUM ('UNSPECIFIED', 'USER', 'MANAGER', 'ADMIN');
     END IF;
-END$$;
+END
+$$;
 
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
