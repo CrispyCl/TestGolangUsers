8128c2b09fb2c14f3d913c4f96154b74e7ba409c
Crispy <chebunin19@gmail.com>
2025-09-28 12:13:13 +0300

    feat(interceptor): add requestID and logging interceptors
    - Add RequestIDInterceptor which creates or obtains requestID from context metadata, and adds it to the context
    - Add LoggingInterceptor which logs grpc requests with duration and method and handles errors

--- internal/transport/grpc/users/interceptor.go
+++ internal/transport/grpc/users/interceptor.go
@@ -2,13 +2,21 @@ package usersgrpc
 
 import (
 	"context"
+	"time"
 
 	"github.com/CrispyCl/TestGolangUsers/pkg/logger"
+	"github.com/google/uuid"
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
+)
+
+const (
+	requestIDKey = logger.RequestIDKey
+	loggerKey    = logger.LoggerKey
 )
 
 func RecoveryInterceptor(ctx context.Context, log logger.Logger) grpc.UnaryServerInterceptor {
@@ -21,4 +29,57 @@ func RecoveryInterceptor(ctx context.Context, log logger.Logger) grpc.UnaryServe
 	}
 
 	return recovery.UnaryServerInterceptor(recoveryOpts...)
+}
+
+func RequestIDInterceptor(baseLog logger.Logger) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+		md, ok := metadata.FromIncomingContext(ctx)
+		var reqID string
+		if ok {
+			if vals := md.Get(string(requestIDKey)); len(vals) > 0 {
+				reqID = vals[0]
+			}
+		}
+		if reqID == "" {
+			reqID = uuid.NewString()
+		}
+
+		log := baseLog.With(zap.String(logger.RequestID, reqID))
+
+		ctx = context.WithValue(ctx, loggerKey, log)
+		ctx = context.WithValue(ctx, requestIDKey, reqID)
+
+		return handler(ctx, req)
+	}
+}
+
+func LoggingInterceptor(baseLog logger.Logger) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+		start := time.Now()
+
+		log, ok := logger.GetLoggerFromContext(ctx)
+		if !ok {
+			log = baseLog
+		}
+
+		log.Info(ctx, "gRPC request: started", zap.String("method", info.FullMethod))
+
+		resp, err = handler(ctx, req)
+
+		duration := time.Since(start)
+		if err != nil {
+			log.Warn(ctx, "gRPC request: finished with error",
+				zap.String("method", info.FullMethod),
+				zap.Duration("duration", duration),
+				zap.Error(err),
+			)
+		} else {
+			log.Info(ctx, "gRPC request: finished",
+				zap.String("method", info.FullMethod),
+				zap.Duration("duration", duration),
+			)
+		}
+
+		return resp, err
+	}
 }
